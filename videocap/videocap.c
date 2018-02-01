4440b97bf12d9b137a9a3f11417546ca1b5e6fd3
Bunji Square <bunji2@users.noreply.github.com>
2018-02-01 22:03:04 +0900

    added gamma correction

--- videocap/videocap.c
+++ videocap/videocap.c
@@ -8,6 +8,36 @@ http://opencv.jp/sample/video_io.html
 #include <opencv/highgui.h>
 #include <ctype.h>
 
+#define GAMMA_TRACKBAR_NAME "gamma"
+
+// ガンマ補正
+// 参考URL http://imagingsolution.blog107.fc2.com/blog-entry-166.html
+IplImage* cvGamma(IplImage* src, int g) {
+    IplImage* dst;
+    uchar LUT[256];
+
+    //printf("#gamma = %d\n", g);
+    dst = cvCreateImage(
+        cvSize(src->width, src->height), 
+        src->depth, 
+        src->nChannels);
+	cvCopy(src, dst, NULL);
+
+    //ガンマ補正テーブルの作成  
+    for (int i = 0; i < 256; i++){  
+        LUT[i] = (int)(pow((double)i / 255.0, 1.0 / (double)g) * 255.0);  
+    }
+
+    //CvMatへ変換  
+    CvMat lut_mat = cvMat(1, 256, CV_8UC1, LUT);  
+
+    //ルックアップテーブル変換  
+    cvLUT(src, dst, &lut_mat);
+
+    return dst;
+}
+
+
 IplImage * filter (IplImage *src_img)
 {
   IplImage *dst_img;
@@ -59,12 +89,12 @@ void cap (
     CvVideoWriter *vw
 ) {
     CvCapture *capture = 0;
-    IplImage *frame = 0, *frame2 = 0;
+    IplImage *frame = 0, *frame2 = 0, *frame3 = 0;
     // (1)コマンド引数によって指定された番号のカメラに対するキャプチャ構造体を作成する
     capture = cvCreateCameraCapture (index);
-    /* この設定は，利用するカメラに依存する */
 
     // (2)キャプチャサイズを設定する．
+    /* この設定は，利用するカメラに依存する */
     cvSetCaptureProperty (capture, CV_CAP_PROP_FRAME_WIDTH, w);
     cvSetCaptureProperty (capture, CV_CAP_PROP_FRAME_HEIGHT, h);
 
@@ -72,26 +102,31 @@ void cap (
     // キー入力があるまで繰り返し
     while (cvWaitKey (wait_ms) == -1) {
         frame = cvQueryFrame (capture);
+        frame2 = cvGamma(frame,  cvGetTrackbarPos(GAMMA_TRACKBAR_NAME, title));
         if (func) {
-            frame2 = func(frame);
+            frame3 = func(frame2);
         }
-        if (!frame2) {
-            frame2 = frame;
+        if (!frame3) {
+            frame3 = frame2;
         }
-        cvShowImage (title, frame2);
+        cvShowImage (title, frame3);
         if (vw) {
-            cvWriteFrame (vw, frame2);
+            cvWriteFrame (vw, frame3);
         }
-        if (frame2 != frame) {
-            cvReleaseImage (&frame2);
+        if (frame3 != frame2) {
+            cvReleaseImage (&frame3);
         }
-        frame2 = 0;
+        cvReleaseImage (&frame2);
+        frame3 = frame2 = 0;
     }
     cvReleaseCapture (&capture);
 }
 
 void videocap(int cam_idx, const char* title, int w, int h, int wait_ms, int use_filter) {
+    int gamma_value = 1;
     cvNamedWindow (title, CV_WINDOW_AUTOSIZE);
+    cvCreateTrackbar(GAMMA_TRACKBAR_NAME, title, &gamma_value, 3, NULL);
+
     if (!use_filter) {
         cap(cam_idx, title, (double)w, (double)h, wait_ms, 0, 0);    
     } else {
